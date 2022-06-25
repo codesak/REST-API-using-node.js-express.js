0c088ab43871b4030530a60172009b38af998f5b
Saksham <95493355+clairocoffin@users.noreply.github.com>
2022-06-25 19:41:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import usersRoutes from "./routes/users.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+
+app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
